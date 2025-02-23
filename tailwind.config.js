3db315b17b3ff4c0d13e5941e7ea5ea0434edf6b
张艳华 <939593210@qq.com>
2025-02-23 19:44:18 +0800

    feat:首次提交

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{ts,tsx}"],
+  important: true,
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  corePlugins: {
+    preflight: false,
+  }
+} 
